583b7988d1b2a34291a428462c6d9aa2c255bfcf
Al Jibran <jibran.web@gmail.com>
2026-06-15 01:30:12 +0530

    site: update RSS description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Al Jibran';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Because I wanted to know how things work';
